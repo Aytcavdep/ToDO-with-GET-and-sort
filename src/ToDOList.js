16aa524fc19238270cd07ed6c2f20cde5b148d05
Kovalenko Vladimir <sazonov2380@gmail.com>
2022-02-25 00:41:12 +0300

    added color separation of tasks by date and completed

--- src/ToDOList.js
+++ src/ToDOList.js
@@ -2,11 +2,31 @@ import TForm from "./TForm";
 
 function ToDOList({ todo, handleChangeCompleted, removeTitle }) {
   const { id, title, completed, userName, date } = todo;
+  const dateNow = new Date();
+  let dayOfMonth = dateNow.getDate();
+  let month = dateNow.getMonth() + 1;
+  let year = dateNow.getFullYear();
+  month = month < 10 ? "0" + month : month;
+  dayOfMonth = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth;
+  let dateOn = `${year}-${month}-${dayOfMonth}`;
+  let dateOff = `${year}-${month}-${dayOfMonth + 7}`;
+  console.log(dateOn);
+
   const handleRemove = () => {
     removeTitle(id);
   };
   return (
-    <div key={id} className="item-todo">
+    <div
+      key={id}
+      className={`${
+        dateOn <= date && date <= dateOff
+          ? "item-todo item-week"
+          : date <= dateOn
+          ? "item-todo item-lost"
+          : "item-todo"
+      }
+    ${completed ? "item-todo item-completed" : ""}`}
+    >
       <div
         className={completed ? "item-text strike" : "item-text"}
         onClick={() => handleChangeCompleted(id)}
